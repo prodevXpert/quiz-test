25e424879a0e5791e7ceb96d30c8230a712038e0
prodevXpert <prodevxpert@gmail.com>
2023-02-26 19:59:36 +0500

    first commit

--- index.js
+++ index.js
@@ -3,14 +3,11 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 
-
-
 const port = process.env.PORT || 5001;
 const app = express();
 app.use(express.static("public"));
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
-
 
 const quizRouter = require("./routes/quizRoutes");
 app.use("/api/quiz", quizRouter);
@@ -18,6 +15,9 @@ app.use("/api/quiz", quizRouter);
 const questionRouter = require("./routes/questionRoutes");
 app.use("/api/question", questionRouter);
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
